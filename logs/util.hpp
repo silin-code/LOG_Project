fbfb926bed7dc38f25ec2f9ea5193e6f25e1e16d
silin-code <2921098172@qq.com>
2026-05-11 19:50:45 +0800

    Initial commit: 项目初始化

---
+++ logs/util.hpp
@@ -0,0 +1,24 @@
+/*
+工具类的实现
+    1获取系统时间
+    2判断文件是否存在
+    3获取文件所在路径
+    4创建目录
+*/
+#include <iostream>
+#include <ctime>
+
+namespace log
+{
+    namespace util
+    {
+        class Date
+        {
+        public:
+            static size_t GetTime()
+            {
+                
+            }
+        };
+    }
+}
